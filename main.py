8ab80432210a34a9b358a2f61e6848f778a472a1
xtine-op <xtineopwapo@gmail.com>
2023-01-23 19:34:42 +0300

    Added Contact page and connected it to the database

--- main.py
+++ main.py
@@ -30,14 +30,44 @@ app.config.update(
     SESSION_COOKIE_HTTPONLY = True,
     SESSION_COOKIE_SAMESITE = 'Lax'
 )
-
+#connecting to the database
+import pymysql
+import html #for html escaping
 @app.route('/')
 def home():
     return render_template('index.html')
+#Comnnecting to the database
+def connection():
+    server = 'localhost'
+    database = 'Tote_254'
+    user = 'root'
+    password = ''
+    conn = pymysql.connect(host = server, user = user, password= password, database=database)
+    return conn
 
-app.route('/contact_page')
+
+@app.route('/contact',  methods=['POST', 'GET'])
 def contact():
-    return  render_template('contact.html')
+    if request.method =='POST':
+        name = html.escape(str(request.form['name']))
+        email = html.escape(str(request.form['email']))
+        subject = html.escape(str(request.form['subject']))
+        comment = html.escape(str(request.form['comment']))
+
+        conn =connection() # change this later to have a variable on its own
+        sql = "Insert into messages(name,email,subject,comment ) values(%s, %s, %s, %s)"
+        cursor = conn.cursor()
+        #try:
+        cursor.execute(sql, (name, email, subject, comment))
+        conn.commit()
+        return render_template('contact.html', msg= 'Thank you for your comment')
+       # except:
+            #return render_template('contact.html', msg= "Failed, Try again later")
+
+    else:
+        return render_template('contact.html')
+
+
 if __name__ == '__main__':
    app.run(debug = True)
 
